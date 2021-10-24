69dbea79956ec33437e1713d3d3b59df7302dc02
weihan <linw14@student.douglascollege.ca>
2021-10-23 22:37:18 -0700

    Render a simple page

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-main_bp = Blueprint("main_bp", __name__)
+main_bp = Blueprint("main_bp", __name__, 
+                    template_folder="templates", 
+                    static_folder="static")
 
 @main_bp.route("/")
 def hello():
-    return "Hello"
+    return render_template("index.jinja2", title="DEMO")
